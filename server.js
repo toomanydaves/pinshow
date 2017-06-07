508e39cc633d64a3384a2286c1c77f2442dbfeec
Sean Jacke <sean.jacke@learnship.com>
2017-06-07 09:39:24 +0200

    configs server to listen on port 3000 by default

--- server.js
+++ server.js
@@ -1,4 +1,11 @@
 var express = require('express');
 var server = express();
+var port = process.env.PORT || 3000;
 
-server.use(express.static('static'));
+server.set('port', port);
+
+server.use(express.static(__dirname + '/static'));
+
+server.listen(port, function () {
+    console.log('Express server is running on port', port);
+});
